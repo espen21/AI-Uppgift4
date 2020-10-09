703a41326260b406911407c3367d648548af265d
espen21 <31931020+espen21@users.noreply.github.com>
2020-10-09 21:41:27 +0200

    woop

--- ai.py
+++ ai.py
@@ -5,6 +5,7 @@ from keras import layers
 from keras import models
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
+import gc
 """
 from keras.models import Sequential
 from keras.utils import np_utils
@@ -68,11 +69,12 @@ def open_csv(PATH):
     x = list(reader)
     del x[0] #removes labels_name,pixels name row
     del (reader)
+    gc.collect()
     return x
 
        
 train_list = open_csv(PATH_TRAIN)
-train_pixels,train_labels = row_to_matrix_train(train_list,20000)
+train_pixels,train_labels = row_to_matrix_train(train_list,2000)
 #print(train_labels)
 
 #test_list = open_csv(PATH_TEST)
@@ -93,22 +95,73 @@ X_Val = X_Val.reshape(X_Val.shape[0], 28, 28, 1)
 print(y_train)
 print(X_train.shape)
 print(y_train.shape)
+"""
 model = models.Sequential()
-model.add(layers.Conv2D(32,(1,1),activation ='relu',input_shape =(28,28,1)))
-model.add(layers.MaxPool2D((2,2)))
-model.add(layers.Conv2D(64,(1,1),activation ='relu'))
-model.add(layers.MaxPool2D((2,2)))
-model.add(layers.Conv2D(128,(1,1),activation ='relu'))
-model.add(layers.MaxPool2D((2,2)))
-model.add(layers.Conv2D(128,(1,1),activation ='relu'))
-model.add(layers.MaxPool2D((2,2)))
+model.add(layers.Conv2D(32,(1,1),activation ='elu',input_shape =(28,28,1)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(64,(1,1),activation ='elu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(128,(1,1),activation ='elu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(128,(1,1),activation ='elu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.BatchNormalization())
 model.add(layers.Flatten())
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(512,activation='relu'))
+model.add(layers.Dropout(0.3))
+model.add(layers.Dense(512,activation='elu'))
 model.add(layers.Dense(1,activation='softmax'))
 model.summary()
+"""
+import keras
+from keras.models import Sequential
+from keras.utils import np_utils
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+from keras.datasets import cifar10
+from keras import regularizers
+from keras.callbacks import LearningRateScheduler
+weight_decay = 1e-4
+num_classes = 10
 
-
+y_train = np_utils.to_categorical(y_train,num_classes)
+y_val = np_utils.to_categorical(y_val,num_classes)
+model = Sequential()
+model.add(Conv2D(32, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay), input_shape=X_train.shape[1:]))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(32, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+ 
+model.add(Conv2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.3))
+ 
+model.add(Conv2D(128, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.4))
+ 
+model.add(Flatten())
+model.add(Dense(num_classes, activation='softmax'))
+ 
+model.summary()
+ 
 model.compile(loss='categorical_crossentropy', optimizer=optimizers.RMSprop(lr=0.001,decay=1e-6), metrics=['accuracy'])
 train_datagen = ImageDataGenerator(rescale=1./255,
                                    rotation_range=40,
@@ -123,7 +176,6 @@ val_datagen = ImageDataGenerator(rescale=1./255)
 train_gen = train_datagen.flow(X_train,y_train,batch_size= BATCH_SiZE)
 val_gen = val_datagen.flow(X_Val,y_val,batch_size=BATCH_SiZE)
 
-print(X_train[0])
 ntrain = len(X_train)
 nval = len(X_Val)
 history = model.fit_generator(train_gen,
@@ -131,4 +183,7 @@ history = model.fit_generator(train_gen,
                               epochs= 64,
                               validation_data= val_gen,
                               validation_steps=nval//BATCH_SiZE)
-                                        
+
+model.save_weights('model_weights.h5')
+model.save("group_24.h5")
+
