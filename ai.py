6727df4817bed10e59a52575b7b13ffbe3084f8d
espen21 <31931020+espen21@users.noreply.github.com>
2020-10-09 18:36:52 +0200

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,34 +1,77 @@
 # -*- coding: utf-8 -*-
 
-import sys
-import os 
-import random
-import gc 
-import numpy as np  
+import keras
+from keras.models import Sequential
+from keras.utils import np_utils
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+from keras import regularizers
+from keras.callbacks import LearningRateScheduler 
+import numpy as np
+import csv 
+#import matplotlib as plt
 import cv2
-import matplotlib.pyplot as plt
-import pandas as pd
-import matplotlib.image as mpimg
-import tensorflow
-
 PATH_TRAIN = "trainset.csv"
 PATH_TEST = "testset.csv"
 PATH_SAMPLE = "samplesubmission.csv" 
 
 print("Hej")
 
+WIDTH =  14
+HEIGHT = 56
+def lr_schedule(epoch):
+    lrate = 0.001
+    if epoch > 75:
+        lrate = 0.0005
+    if epoch > 100:
+        lrate = 0.0003
+    return lrate
 
-from keras.datasets import cifar10
-def show_imgs(X):
-    plt.figure(1)
-    k = 0
-    for i in range(0,4):
-        for j in range(0,4):
-            plt.subplot2grid((4,4),(i,j))
-            plt.imshow(toimage(X[k]))
-            k = k+1
-    # show the plot
-    plt.show()
+def row_to_matrix_train(list1,amount_image):
+    result = []
+    labels = []
+    i = 0
+    for row in list1:
+        labels.append(row[0])
+        del row[0]
+        row = map(float,row)
+        row = list(row)
+        matrix = np.asmatrix(row)
+        matrix= matrix.reshape(28,28)
+        result.append(matrix)
+        i +=1
+        if i > amount_image:
+            break
+    return result,labels    
+
+
+def open_csv(PATH):
+    reader = csv.reader(open(PATH, "rt",encoding= 'utf-8'), delimiter=",")
+    x = list(reader)
+    del x[0] #removes labels_name,pixels name row
+    return x
+
+       
+train_list = open_csv(PATH_TRAIN)
+train_pixels,train_labels = row_to_matrix_train(train_list,10)
+print(type(train_pixels))
+#print(train_labels)
+
+#test_list = open_csv(PATH_TEST)
+#test_pixels,test_labels = row_to_matrix(test_list)
+
+#mean = np.mean(train_pixels,axis=(0,1,2,3))
+#std = np.std(train_pixels,axis=(0,1,2,3))
+#train_pixels = (train_pixels-mean)/(std+1e-7)
+#x_test = (x_test-mean)/(std+1e-7)
  
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-show_imgs(x_test[:16])
+from PIL import Image
+#print((train_pixels[0]))
+print(train_pixels[0])
+print(train_labels)
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+plt.imshow(train_pixels[0], cmap='gray', vmin=0, vmax=255)
+plt.show()
